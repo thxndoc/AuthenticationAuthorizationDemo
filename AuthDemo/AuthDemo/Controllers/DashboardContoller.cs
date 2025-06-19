1639a66fb1dfc82e40eb6e701a0495821c7e8059
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-06-19 19:48:56 +0200

    feature: create DashboardController with role-based authorization for User and Admin views

---
+++ AuthDemo/AuthDemo/Controllers/DashboardContoller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthDemo.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles = "User")]
+        public IActionResult User() => View();
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Admin() => View();
+    }
+}
